ade80cfc59ca9d2a15c46262ccad482ef475fece
donaldking <donaldk@objecttechgroup.com>
2020-01-26 17:50:32 +0000

    Initial Commit

---
+++ HospitalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HospitalApp
+//
+//  Created by Donald King on 26/01/2020.
+//  Copyright © 2020 Donald King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
