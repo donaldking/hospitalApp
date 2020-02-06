3de6054c6599a73dab69d094a53f3cc869aef836
Donald King <donaldking247@gmail.com>
2020-02-06 13:31:54 +0000

    Minor code refactoring

--- HospitalApp/Helpers/HospitalService.swift
+++ HospitalApp/Helpers/HospitalService.swift
@@ -9,12 +9,62 @@
 import Foundation
 
 public class HospitalService: ServiceRequestable, ServiceParsable {
-    // MARK: - Protocol conformance
+    public typealias T = [Hospital]
+
     public var endPoint: URL {
         guard let url = URL(string: "https://media.nhschoices.nhs.uk/data/foi/Hospital.csv") else { fatalError("Unable to prepare URL")}
         return url
     }
-    
+
+    public func requestData(completed: @escaping ([Hospital]?) -> Void) {
+                DispatchQueue.global().async { [weak self] in
+            guard let self = self else { completed(nil); return }
+            let task = URLSession.shared.downloadTask(with: self.endPoint) { localURL, urlResponse, error in
+                // Load local data if error getting remote data
+                if let _ = error {
+                    self.loadLocalData {(data) in
+                        if let data = data {
+                            self.parse(data: data) { (hospitals) in
+                                DispatchQueue.main.async {
+                                    completed(hospitals)
+                                }
+                            }
+                        } else {
+                            completed(nil)
+                        }
+                    }
+                }
+                // Check for correct response
+                if let urlResponse = urlResponse {
+                    let response = urlResponse as! HTTPURLResponse
+                    switch (response.statusCode) {
+                    case 200 ... 299:
+                        if let localURL = localURL {
+                            if let data = try? Data(contentsOf: localURL) {
+                                self.parse(data: data) { (hospitals) in
+                                    DispatchQueue.main.async {
+                                        completed(hospitals)
+                                    }
+                                }
+                            } else {
+                                DispatchQueue.main.async {
+                                    completed(nil)
+                                }
+                            }
+                        }
+                    default:
+                        self.loadLocalData {(data) in
+                            DispatchQueue.main.async {
+                                completed(nil)
+                            }
+                        }
+                        break
+                    }
+                }}
+            task.resume()
+        }
+    }
+
     public func parse(data: Data, completed: @escaping ([Hospital]?) -> Void) {
         DispatchQueue.global().async {
             if let csvString = String(data: data, encoding: .ascii) {
@@ -34,50 +84,7 @@ public class HospitalService: ServiceRequestable, ServiceParsable {
             }
         }
     }
-    
-    public func requestData(completed: @escaping (Data?) -> Void) {
-        DispatchQueue.global().async { [weak self] in
-            guard let self = self else { completed(nil); return }
-            let task = URLSession.shared.downloadTask(with: self.endPoint) { localURL, urlResponse, error in
-                // Load local data if error getting remote data
-                if let _ = error {
-                    self.loadLocalData {(data) in
-                        DispatchQueue.main.async {
-                            completed(data)
-                        }
-                    }
-                }
-                // Check for correct response
-                if let urlResponse = urlResponse {
-                    let response = urlResponse as! HTTPURLResponse
-                    switch (response.statusCode) {
-                    case 200 ... 299:
-                        if let localURL = localURL {
-                            if let data = try? Data(contentsOf: localURL) {
-                                DispatchQueue.main.async {
-                                    completed(data)
-                                }
-                            } else {
-                                self.loadLocalData {(data) in
-                                    DispatchQueue.main.async {
-                                        completed(data)
-                                    }
-                                }
-                            }
-                        }
-                    default:
-                        self.loadLocalData {(data) in
-                            DispatchQueue.main.async {
-                                completed(data)
-                            }
-                        }
-                        break
-                    }
-                }}
-            task.resume()
-        }
-    }
-    
+
     // MARK: - Private methods
     private func loadLocalData(_ completed:@escaping(Data?) -> Void) {
         DispatchQueue.global(qos: .userInitiated).async {
